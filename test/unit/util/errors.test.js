2ebade34a0867a23ae4a9b4013e4887ab0880bf6
antgroz <antongroznykh@gmail.com>
2022-04-24 18:10:20 +0300

    Error tests

--- test/unit/util/errors.test.js
+++ test/unit/util/errors.test.js
@@ -1,23 +1,22 @@
-'use strict';
+import * as errors from '../../../lib/util/errors.js';
+import * as chai from 'chai';
+chai.should();
 
-var errors = require('../../../lib/util/errors');
-require('chai').should();
+describe('errors', () => {
+  for (const name in errors) {
+    const E = errors[name];
 
-describe('errors', function () {
-  for (var name in errors) {
-    var E = errors[name];
-
-    describe(name, function () {
-      it('should have a correct name', function () {
+    describe(name, () => {
+      it('should have a correct name', () => {
         new E().name.should.eq(name);
       });
 
-      it('should have a stack trace', function () {
+      it('should have a stack trace', () => {
         new E().stack.should.not.be.undefined;
       });
 
-      it('should accept a message', function () {
-        var message = 'foo';
+      it('should accept a message', () => {
+        const message = 'foo';
         new E(message).message.should.eq(message);
       });
     });
