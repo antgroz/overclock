9c07cbd607073b3941c33038d2f04a6886e3b522
antgroz <antongroznykh@gmail.com>
2022-04-26 01:33:52 +0300

    Bring back type checking utilities

---
+++ test/unit/is.test.js
@@ -0,0 +1,53 @@
+import { describe, it } from 'mocha';
+import {
+  isNullishOrFiniteNonNegativeNumber,
+  isNullishOrFiniteNumber,
+  isNullishOrInteger,
+} from '../../lib/is.js';
+import { should } from 'chai';
+
+should();
+
+describe('is', () => {
+  describe('is nullish or integer', () => {
+    for (const value of [null, undefined, 3, 0, -2]) {
+      it(`should return true for ${value}`, () => {
+        isNullishOrInteger(value).should.be.true;
+      });
+    }
+
+    for (const value of ['foo', -2.332, Number.POSITIVE_INFINITY, 1.1]) {
+      it(`should return false for ${value}`, () => {
+        isNullishOrInteger(value).should.be.false;
+      });
+    }
+  });
+
+  describe('is nullish or finite non-negative number', () => {
+    for (const value of [null, undefined, 3.2, 0, 2]) {
+      it(`should return true for ${value}`, () => {
+        isNullishOrFiniteNonNegativeNumber(value).should.be.true;
+      });
+    }
+
+    for (const value of ['foo', -2.332, Number.POSITIVE_INFINITY]) {
+      it(`should return false for ${value}`, () => {
+        isNullishOrFiniteNonNegativeNumber(value).should.be.false;
+      });
+    }
+  });
+
+  describe('is nullish or finite number', () => {
+    for (const value of [null, undefined, 3.2, 0, 2, -2, -9.23]) {
+      it(`should return true for ${value}`, () => {
+        isNullishOrFiniteNumber(value).should.be.true;
+      });
+    }
+
+    for (const value of ['foo', Number.NEGATIVE_INFINITY]) {
+      it(`should return false for ${value}`, () => {
+        isNullishOrFiniteNumber(value).should.be.false;
+      });
+    }
+  });
+});
