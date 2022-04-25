5c26735508a34be3677abc4460634778f9d5138f
antgroz <antongroznykh@gmail.com>
2022-04-26 01:51:07 +0300

    Check heartbeat task options

--- test/unit/heartbeat.test.js
+++ test/unit/heartbeat.test.js
@@ -10,20 +10,36 @@ describe('heartbeat', () => {
   const clock = useFakeTimers();
 
   describe('constructor', () => {
+    it('should check that heartbeat interval is valid', () => {
+      const options = {
+        name: 'foo',
+        executable: () => 0,
+        heartbeatIntervalMillis: -2,
+      };
+      const ctor = () => new Heartbeat(options);
+      const message =
+        'Task heartbeat interval must be a finite non-negative number';
+      ctor.should.throw(message);
+    });
+
     it('should set up additional parameters', () => {
-      const task = new Heartbeat({ heartbeatIntervalMillis: 300 });
+      const task = new Heartbeat({
+        name: 'foo',
+        executable: () => null,
+        heartbeatIntervalMillis: 300,
+      });
       task.heartbeatIntervalMillis.should.eq(300);
     });
 
     it('should use defaults when necessary', () => {
-      const task = new Heartbeat({});
+      const task = new Heartbeat({ name: 'foo', executable: () => null });
       task.heartbeatIntervalMillis.should.eq(1000);
     });
   });
 
   describe('_start', () => {
     it('should set up the heartbeat interval', () => {
-      const task = new Heartbeat({});
+      const task = new Heartbeat({ name: 'foo', executable: () => null });
       task._spawn = () => {};
       task._start();
       task._interval.should.not.be.null;
@@ -32,7 +48,7 @@ describe('heartbeat', () => {
     });
 
     it('should call _spawn method right away', () => {
-      const task = new Heartbeat({});
+      const task = new Heartbeat({ name: 'foo', executable: () => null });
       task._spawn = spy();
       task._start();
       task._spawn.calledOnce.should.be.true;
@@ -40,7 +56,7 @@ describe('heartbeat', () => {
     });
 
     it('should call parent _start method', () => {
-      const task = new Heartbeat({});
+      const task = new Heartbeat({ name: 'foo', executable: () => null });
       task._spawn = () => {};
       Base.prototype._start = spy();
       task._start();
@@ -51,14 +67,14 @@ describe('heartbeat', () => {
 
   describe('_tryStop', () => {
     it('should call the parent _tryStop method', async () => {
-      const task = new Heartbeat({});
+      const task = new Heartbeat({ name: 'foo', executable: () => null });
       Base.prototype._tryStop = spy();
       await task._tryStop();
       Base.prototype._tryStop.calledOnce.should.be.true;
     });
 
     it('should clean up heartbeat interval', async () => {
-      const task = new Heartbeat({});
+      const task = new Heartbeat({ name: 'foo', executable: () => null });
       task._interval = setInterval(() => null, 1000);
       Base.prototype._tryStop = async () => {
         expect(task._interval).to.be.null;
