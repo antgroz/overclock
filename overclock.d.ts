e91d45b6392f82502b9a774bf2154b3c1ffcd0c5
antgroz <antongroznykh@gmail.com>
2022-04-26 16:36:11 +0300

    Finish types

--- overclock.d.ts
+++ overclock.d.ts
@@ -1,95 +1,89 @@
-export type TaskType = 'heartbeat' | 'reactor';
-export type Task<R = any> = HeartbeatTask<R> | ReactorTask<R>;
-export type TaskOptions<R = any> = {
-  type: TaskType;
-} & HeartbeatTaskOptions<R> &
-  ReactorTaskOptions<R>;
-export type TaskListener = (...args: unknown[]) => void;
-
-export type PrincipeOptions = {
-  tasks?: TaskOptions[];
-};
-
-export type BaseTaskOptions<R> = {
-  name: string;
-  executable: () => R | Promise<R>;
-  concurrencyLimit: number;
-  livenessThreshold: number;
-  factoryCapacity: number;
-  generationLimit: number;
-  startTimeoutMillis: number;
-  runTimeoutMillis: number;
-  stopTimeoutMillis: number;
-  gracefulTimeoutMillis: number;
-};
 import { EventEmitter } from 'node:events';
 
-
-export type HeartbeatTaskOptions<R = any> = BaseTaskOptions<R> & {
-  heartbeatIntervalMillis: number;
-};
-
-export type ReactorTaskOptions<R> = BaseTaskOptions<R> & {
-  reactorTimeoutMillis: number;
-};
-
-
-export type Schedule = 'once' | 'periodic' | 'recurrent' | 'sequential';
+export type TaskType = 'heartbeat' | 'reactor';
 
 export type TaskEvent =
   | 'starting'
   | 'started'
+  | 'spawning'
+  | 'spawned'
   | 'tick'
   | 'tock'
   | 'stopping'
   | 'stopped';
 
-export type TaskExecutable<R = any> =
-  | (() => R)
-  | ((callback: (error: Error | null, result: null | R) => void) => void)
-  | (() => PromiseLike<R>);
+export type TaskExecutable<R = any> = () => R | Promise<R>;
 
-export type TaskOptions<R = any> = {
-  name: string;
-  executable: TaskExecutable<R>;
-  schedule?: Schedule;
-  unpipe?: boolean | TaskEvent | TaskEvent[];
-  startTimeoutMillis?: number;
-  runIntervalMillis?: number;
-  stopTimeoutMillis?: number;
-  gracefulTimeoutMillis?: number;
-  Promise?: PromiseConstructorLike;
+export type TaskDataTick<R = any> = {
+  task: Task<R>;
+  tickAt: Date;
+  tockAt: null;
+  error: null;
+  result: null;
 };
 
-export type TaskData<R = any> = {
+export type TaskDataTock<R = any> = {
   task: Task<R>;
   tickAt: Date;
   tockAt: Date;
-  error: Error;
-  result: R;
+  error: Error | null;
+  result: null | R;
 };
+
+export type TaskDataSpawned<R = any> = {
+  task: Task<R>;
+  error: Error | null;
+  result: null | number;
+};
+
+export type TaskDataOther<R = any> = {
+  task: Task<R>;
+};
+
+export type TaskData<R> =
+  | TaskDataTick<R>
+  | TaskDataTock<R>
+  | TaskDataSpawned<R>
+  | TaskDataOther<R>;
 
 export type TaskListener<R = any> = (data: TaskData<R>) => void;
 
-declare class Task<R = any> extends EventEmitter {
+export type BaseTaskOptions<R = any> = {
   name: string;
   executable: TaskExecutable<R>;
-  startTimeoutMillis?: string;
-  runIntervalMillis?: string;
-  stopTimeoutMillis?: string;
-  gracefulTimeoutMillis?: string;
-  Promise?: PromiseConstructorLike;
-  isStarting: boolean;
-  isStarted: boolean;
-  runningCount: number;
-  isStopping: boolean;
-  isStopped: boolean;
-  startingAt?: Date;
-  startedAt?: Date;
-  stoppingAt?: Date;
-  stoppedAt?: Date;
+  concurrencyLimit?: number;
+  livenessThreshold?: number;
+  factoryCapacity?: number;
+  generationLimit?: number;
+  startTimeoutMillis?: number;
+  runTimeoutMillis?: number;
+  stopTimeoutMillis?: number;
+  gracefulTimeoutMillis?: number;
+};
 
-  constructor(options: TaskOptions<R>);
+declare class Base<R = any> extends EventEmitter {
+  readonly name: string;
+  readonly executable: TaskExecutable<R>;
+  readonly concurrencyLimit: number;
+  readonly livenessThreshold: number;
+  readonly factoryCapacity: number;
+  readonly generationLimit: number;
+  readonly startTimeoutMillis: number;
+  readonly runTimeoutMillis: number;
+  readonly stopTimeoutMillis: string;
+  readonly gracefulTimeoutMillis: string;
+  readonly isStarting: boolean;
+  readonly isStarted: boolean;
+  readonly isStopping: boolean;
+  readonly isStopped: boolean;
+  readonly startingAt: Date | null;
+  readonly startedAt: Date | null;
+  readonly stoppingAt: Date | null;
+  readonly stoppedAt: Date | null;
+  readonly population: number;
+  readonly generations: number;
+  readonly ticks: number;
+  readonly tocks: number;
 
   addListener(event: TaskEvent, listener: TaskListener): this;
 
@@ -105,31 +99,42 @@ declare class Task<R = any> extends EventEmitter {
 
   start(): void;
 
-  stop(): PromiseLike<void>;
-  stop(callback: (error: Error | null) => void): void;
+  stop(): Promise<void>;
 }
 
-export type RacerOptions = {
-  schedule?: Schedule;
-  startTimeoutMillis?: number;
-  runIntervalMillis?: number;
-  stopTimeoutMillis?: number;
-  gracefulTimeoutMillis?: number;
-  Promise?: PromiseConstructorLike;
-  tasks: Task[];
+export type HeartbeatTaskOptions<R = any> = BaseTaskOptions<R> & {
+  heartbeatIntervalMillis?: number;
 };
 
-declare class Racer extends EventEmitter {
-  schedule: Schedule;
-  startTimeoutMillis?: number;
-  runIntervalMillis?: number;
-  stopTimeoutMillis?: number;
-  gracefulTimeoutMillis?: number;
-  Promise?: PromiseConstructorLike;
-  tasks: Record<string, Task>;
-  pipes: Record<TaskEvent, TaskListener>;
+declare class Heartbeat<R = any> extends Base<R> {
+  readonly heartbeatIntervalMillis: number;
 
-  constructor(options?: RacerOptions);
+  constructor(options: HeartbeatTaskOptions<R>);
+}
+
+export type ReactorTaskOptions<R = any> = BaseTaskOptions<R> & {
+  reactorTimeoutMillis?: number;
+};
+
+declare class Reactor<R = any> extends Base<R> {
+  readonly reactorTimeoutMillis: number;
+
+  constructor(options: ReactorTaskOptions<R>);
+}
+
+export type TaskOptions<R = any> = { type: TaskType } & (
+  | HeartbeatTaskOptions<R>
+  | ReactorTaskOptions<R>
+);
+
+export type Task<R = any> = Heartbeat<R> | Reactor<R>;
+
+export type ManagerOptions = {
+  tasks?: TaskOptions[];
+};
+
+declare class Manager extends EventEmitter {
+  constructor(options?: ManagerOptions);
 
   addListener(event: TaskEvent, listener: TaskListener): this;
 
@@ -149,18 +154,17 @@ declare class Racer extends EventEmitter {
 
   has(task: string): boolean;
 
-  del(task: string): boolean;
+  delete(task: string): boolean;
 
-  pipe(task: string, event?: TaskEvent): this;
+  subscribe(task: string, event?: TaskEvent): this;
 
-  unpipe(task: string, event?: TaskEvent): this;
+  unsubscribe(task: string, event?: TaskEvent): this;
 
   start(task?: string): this;
 
-  stop(task?: string): PromiseLike<void>;
-  stop(callback: (error: Error | null) => void, task?: string): void;
+  stop(task?: string): Promise<void>;
 }
 
-export function taskracer(options?: RacerOptions): Racer;
+export function overclock(options?: ManagerOptions): Manager;
 
-export default taskracer;
+export default overclock;
